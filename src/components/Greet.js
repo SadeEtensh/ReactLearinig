6cc278a60aa53f728a43cddd9f38a20e87f55823
SadeEtensh <eteneshsade550305@gmail.com>
2023-09-16 14:05:21 +0300

    React Learining

---
+++ src/components/Greet.js
@@ -0,0 +1,10 @@
+export const Greet = () => {
+  return <h1> Hello Sade</h1>;
+};
+
+// export default Greet;
+// Defalt export- U can import the Componet with any Name U like
+
+// Named Export
+// if we append export to function export const funcName---we have to import the func with exact name
+// we have to destructure import
